d8d9ecaa93a748c1f243287fc9b93b75a23ad08a
costrella <misiek.mk@gmail.com>
2017-11-08 16:07:58 +0100

    RouterModule.forRoot

--- ng-app/src/app/app.module.ts
+++ ng-app/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import { ItemsComponent } from './components/items/items.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { WorkersComponent } from './components/workers/workers.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    RegisterFormComponent,
+    WorkersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'items', component: ItemsComponent},
+      {path: 'register-form', component: RegisterFormComponent},
+      {path: 'workers', component: WorkersComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
